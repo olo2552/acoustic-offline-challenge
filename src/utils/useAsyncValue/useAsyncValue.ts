389b65c656c3ac69cfef058448f45811afa29b6f
awielgorski <a.wielgorski@protonmail.com>
2020-07-25 22:13:45 +0200

    useAsyncValue hook

---
+++ src/utils/useAsyncValue/useAsyncValue.ts
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+
+function delay(t: any, v: any) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve.bind(null, v), t)
+    });
+}
+
+// the generic parameter could be omitted, if I used infer TS keyword, with strict type safety
+// but from the experience it can be problematic and can take some time to do it right, so I leave it as is for now
+export const useAsyncValue = <TPromisedValue>(promisedValue: Promise<TPromisedValue>) => {
+    const [asyncValue, setAsyncValue] = useState<TPromisedValue | null>(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            console.log("effect called")
+            try {
+                setIsLoading(true)
+                const response = await promisedValue;
+                await delay(2000, {})
+                setAsyncValue(response);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        })();
+    }, []);
+    return { asyncValue, error, isLoading };
+};
