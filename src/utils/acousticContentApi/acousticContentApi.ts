4707793d560cc0d4779709727fac8218168f1301
awielgorski <a.wielgorski@protonmail.com>
2020-07-25 22:11:54 +0200

    acousticContentApi

---
+++ src/utils/acousticContentApi/acousticContentApi.ts
@@ -0,0 +1,10 @@
+import Axios, {AxiosResponse} from "axios";
+import {IAcousticContentArticle} from "./acousticContentApi.types";
+
+const getArticle = (articleId: string): Promise<AxiosResponse<IAcousticContentArticle>> => {
+    return Axios.get<IAcousticContentArticle>(`https://content-eu-4.content-cms.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/content/${articleId}`);
+};
+
+export const AcousticContentApi = {
+    getArticle,
+};
