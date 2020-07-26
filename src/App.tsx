510d248a7c1da340da77ff6fba318016cbef6416
awielgorski <a.wielgorski@protonmail.com>
2020-07-26 15:37:16 +0200

    remove divity

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 import {Article} from "./Article/Article";
 
 function App() {
   return (
-    <div className="App">
       <Article articleId={"fa9519d5-0363-4b8d-8e1f-627d802c08a8"}/>
-    </div>
   );
 }
 
