2eff1616a07760d2040145b04d3666c2ee0e3c29
awielgorski <a.wielgorski@protonmail.com>
2020-07-26 15:37:31 +0200

    WIP show only spinner

--- src/Article/Article.tsx
+++ src/Article/Article.tsx
@@ -1,10 +1,21 @@
+/** @jsx jsx */
 import React, {useMemo} from "react";
 import {useAsyncValue} from "../utils/useAsyncValue/useAsyncValue";
 import {AcousticContentApi} from "../utils/acousticContentApi/acousticContentApi";
 import {Spinner} from "../Spinner/Spinner";
+import {ArticleSection} from "./ArticleSection";
+import {css, jsx} from "@emotion/core";
+import {LabelWithIcon} from "./LabelWithIcon";
+import {formatDistanceToNow} from "date-fns";
 
 interface IArticleProps {
     articleId: string;
+}
+
+const getArticleCreationDate = (isoDate: string): string => {
+    return formatDistanceToNow(new Date(isoDate), {
+        addSuffix: true
+    });
 }
 
 export const Article: React.FC<IArticleProps> = (props) => {
@@ -15,23 +26,64 @@ export const Article: React.FC<IArticleProps> = (props) => {
         isLoading: isArticleLoading,
     } = useAsyncValue(articlePromise);
 
-    console.log({isArticleLoading, articleError, asyncArticle})
-
-    if (isArticleLoading) {
+    if (true) {
         return (
             <Spinner/>
         );
     }
 
-    if (articleError) {
-        return (
-          <p>{"something went wrong"}</p>
-        );
-    }
-
-    return (
-        <div>
-            {JSON.stringify(asyncArticle)}
-        </div>
-    );
+    // if (articleError) {
+    //     return (
+    //       <p>{"something went wrong"}</p>
+    //     );
+    // }
+    //
+    // return (
+    //     <article
+    //         css={css`
+    //             max-width: 760px;
+    //             margin: 0 auto;
+    //         `}
+    //         className={"content columns is-vcentered"}
+    //     >
+    //         <img
+    //             style={{
+    //                 width: "100%"
+    //             }}
+    //             src={AcousticContentApi.getImageLink(asyncArticle?.elements.mainImage.value.leadImage.renditions.lead.url)}
+    //             alt={asyncArticle?.elements.mainImage.value.leadImageCaption.value}
+    //         />
+    //         <h1
+    //             className={"title is-1"}
+    //             css={css`
+    //                 font-size: 4rem
+    //             `}>
+    //             {asyncArticle?.elements.heading.value}
+    //         </h1>
+    //         <section css={css`
+    //             width: 100%;
+    //             display: flex;
+    //             justify-content: space-between;
+    //         `}>
+    //             <LabelWithIcon
+    //                 label={asyncArticle?.elements.author.value || 'Author'}
+    //                 icon={"user"}
+    //             />
+    //             {asyncArticle?.elements.date.value && (
+    //                 <LabelWithIcon
+    //                     label={getArticleCreationDate(asyncArticle?.elements.date.value)}
+    //                     icon={"calendar"}
+    //                 />
+    //             )}
+    //         </section>
+    //         {asyncArticle?.elements.body.values.map((articleSectionContent, sectionIndex) => {
+    //             return (
+    //                 <ArticleSection
+    //                     key={sectionIndex}
+    //                     content={articleSectionContent}
+    //                 />
+    //             );
+    //         })}
+    //     </article>
+    // );
 }
