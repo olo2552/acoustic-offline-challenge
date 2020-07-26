101272906afed7ca381bc9096db71483278c48e3
awielgorski <a.wielgorski@protonmail.com>
2020-07-26 15:34:51 +0200

    spinner is black and ceneterd

--- src/Spinner/Spinner.tsx
+++ src/Spinner/Spinner.tsx
@@ -1,8 +1,22 @@
+/** @jsx jsx */
 import React from 'react';
 import LoaderSpinner from 'react-loader-spinner';
+import {css, jsx} from "@emotion/core";
 
 export const Spinner: React.FC = () => {
     return (
-        <LoaderSpinner type={"Bars"} visible />
+        <main css={css`
+            height: 100%;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        `}>
+            <LoaderSpinner
+                color={"#000"}
+                type={"Bars"}
+                visible
+            />
+        </main>
     )
 }
