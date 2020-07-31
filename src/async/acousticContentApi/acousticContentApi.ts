9515fb60cfa5488c5337bb8d8164d7349d7d4be0
awielgorski <a.wielgorski@protonmail.com>
2020-07-31 13:46:41 +0200

    semicolon

--- src/async/acousticContentApi/acousticContentApi.ts
+++ src/async/acousticContentApi/acousticContentApi.ts
@@ -5,7 +5,7 @@ const getArticle = (articleId: string): Promise<IAcousticContentArticle> => {
     return Axios.get<IAcousticContentArticle>(`https://content-eu-4.content-cms.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/content/${articleId}`)
         .then((response) => {
             return response.data;
-        });
+        })
 };
 
 const getImageLink = (imageSlug?: string): string => {
